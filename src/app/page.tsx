411c5dd48b6b44fa3ef5a16e74bc2e8123d12ac7
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-05-22 00:07:40 -0300

    feat(page): Added Slide1 component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import Header from '@src/components/Header'
+import Slide1 from '@src/components/Slides/Slide1'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <main className="flex flex-col"></main>
+      <main className="flex flex-col">
+        <Slide1 />
+      </main>
     </>
   )
 }
