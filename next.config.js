0b09f28573e6a8c3542c8a012930bcc9f35f81d0
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-05-21 23:06:08 -0300

    chore(next): Added environment variables to Next.js configuration file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    SOURCE_CODE_URL: process.env.SOURCE_CODE_URL,
+    SITE_URL: process.env.SITE_URL,
+    AUTHOR: process.env.AUTHOR
+  }
 }
 
 module.exports = nextConfig
