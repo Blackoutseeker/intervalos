f7ee0e39e7f9b06eecd04f96cea82057f8e39be3
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-05-23 02:38:37 -0300

    feat(pages): Added Slide4 component to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from '@src/components/Header'
 import Slide1 from '@src/components/Slides/Slide1'
 import Slide2 from '@src/components/Slides/Slide2'
 import Slide3 from '@src/components/Slides/Slide3'
+import Slide4 from '@src/components/Slides/Slide4'
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
         <Slide1 />
         <Slide2 />
         <Slide3 />
+        <Slide4 />
       </main>
     </>
   )
